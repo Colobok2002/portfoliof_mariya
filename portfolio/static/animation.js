415d65d192a8769d6d0a6039541a55967e1db014
ilyab <ilya.bv.2002@bk.ru>
2023-03-27 00:45:50 +0300

    Перенес на жданго

--- portfolio/static/animation.js
+++ portfolio/static/animation.js
@@ -2,18 +2,17 @@ function onEntry(entry) {
     entry.forEach(change => {
       if (change.isIntersecting) {
        change.target.classList.add('element-show');
-      } else {
+      }/*else {
         change.target.classList.remove('element-show');
-      }
-    });
+      }*/
+  });
   }
   
 //let options = {threshold: [0.00, 0.01, 0.02, /*…,*/ 0.99, 1.00]};
-let options = {threshold: [0.4]};
+let options = {threshold: [0.3]};
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('section , .header__text , .about__content, .header__text-box.row , .my_work');
-console.log(elements);
-  
+
 for (let elm of elements) {
     observer.observe(elm);
   }
